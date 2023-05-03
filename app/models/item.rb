adc6a5a3745ff0dd84cc2162787fdc345f46631b
Yamasaki <88619524+Yamasaki0401@users.noreply.github.com>
2023-05-03 23:00:17 +0900

    ActiveHash追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,14 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :condition
+  belongs_to :category
+  belongs_to :shipping_fee
+  belongs_to :shipping_schedule
+  belongs_to :prefecture
   belongs_to :user
+
+  validates :title, :text, presence: true
+  validates :genre_id, numericality: { other_than: 1 }
+  validates :genre_id, numericality: { other_than: 1 , message: "can't be blank"}
+
 end
