3224f8a63c55ca8e2a88ba3363ca138f7c53e5dd
Yamasaki <88619524+Yamasaki0401@users.noreply.github.com>
2023-05-05 19:29:34 +0900

    テストコード修正1

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,6 +13,5 @@ class Item < ApplicationRecord
             :shipping_fee_id, :shipping_schedule_id, :prefecture_id, presence: true
   validates :condition_id, :category_id, :shipping_fee_id, :shipping_schedule_id, :prefecture_id,
             numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
-                    format: { with: /\A[0-9]+\z/ }
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,24 +4,27 @@ RSpec.describe Item, type: :model do
   before do
     @item = FactoryBot.build(:item)
   end
+
   describe '商品出品' do
     context '商品登録ができる' do
-      it 'imageとitem_name,price,description,condition_id,category_id,
-      shipping_fee_id,shipping_schedule_id,prefectureを登録できなる' do
-        expect(@item).to be_valid
-      end
-
-      it 'priceが300~9999999だと登録できる' do
+      it 'imageとitem_name,price,description,condition_id,category_id,shipping_fee_id,shipping_schedule_id,prefectureを登録できる' do
         expect(@item).to be_valid
       end
 
       it 'priceが半角数値であれば登録できる' do
         expect(@item).to be_valid
       end
+
+      it 'priceが300~9999999だと登録できる' do
+        expect(@item).to be_valid
+      end
     end
 
     context '商品登録ができない' do
       it 'imageが空だと登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
       end
 
       it 'item_nameが空だと登録できない' do
@@ -42,6 +45,12 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
 
+      it 'condition_idが空だと登録できない' do
+        @item.condition_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+
       it 'condition_idが1だと登録できない' do
         @item.condition_id = 1
         @item.valid?
@@ -54,14 +63,32 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
 
+      it 'category_idが空だと登録できない' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+
       it 'shippinge_fee_idが1だと登録できない' do
         @item.shipping_fee_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping fee can't be blank")
       end
 
+      it 'shipping_fee__idが空だと登録できない' do
+        @item.shipping_fee_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping fee can't be blank")
+      end
+
       it 'shipping_schedule_idが1だと登録できない' do
         @item.shipping_schedule_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping schedule can't be blank")
+      end
+
+      it 'shipping_schedule_idが空だと登録できない' do
+        @item.shipping_schedule_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping schedule can't be blank")
       end
@@ -88,6 +115,12 @@ RSpec.describe Item, type: :model do
         @item.price = '３０００'
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+
+      it 'prefecture_idが空だと登録できない' do
+        @item.prefecture_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
 
       it 'ユーザーが紐付いていなければ投稿できない' do
