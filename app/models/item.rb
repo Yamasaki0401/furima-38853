1350626205d1f0105c1a693f79d40bad536a0e1a
Yamasaki <88619524+Yamasaki0401@users.noreply.github.com>
2023-05-31 16:28:11 +0900

    複数画像投稿機能実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,13 +7,14 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :user
 
-  has_one_attached :image
+  has_many_attached :images
   has_one :order
   has_many :comments, dependent: :destroy
 
-  validates :image, :item_name, :price, :description, :condition, :category_id,
+  validates :images, :item_name, :price, :description, :condition, :category_id,
             :shipping_fee_id, :shipping_schedule_id, :prefecture_id, presence: true
   validates :condition_id, :category_id, :shipping_fee_id, :shipping_schedule_id, :prefecture_id,
             numericality: { other_than: 1, message: "can't be blank" }
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :images, length: { minimum: 1, maximum: 5 }
 end
